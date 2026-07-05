1fa77414f04c4cd061273b992e079adf142e1510
Moh_Jaber_Ziaee <ziaeejaber@gmail.com>
2026-07-05 15:15:03 +0430

    Fixing image
    Fix

--- src/Right.jsx
+++ src/Right.jsx
@@ -8,10 +8,10 @@ function Right() {
                 <h1 className='p-8 text-2xl font-bold'>Summary</h1>
             </section>
             <section className='w-full h-8/10 flex flex-col justify-around items-center'>
-                <InnerComp image={'assets/images/icon-reaction.svg'} name={'Reaction'} info={'80/100'} Color="hsl(0, 100%, 67%, 30%)" />
-                <InnerComp image={'assets/images/icon-memory.svg'} name={'Memory'} info={'96/100'} Color="hsl(39, 100%, 56%, 30%)" />
-                <InnerComp image={'assets/images/icon-verbal.svg'} name={'Varbal'} info={'61/100'} Color="hsl(166, 100%, 37%, 30%)" />
-                <InnerComp image={'assets/images/icon-visual.svg'} name={'Visual'} info={'73/100'} Color="hsl(234, 85%, 45%, 30%)" />
+                <InnerComp image={'/assets/images/icon-reaction.svg'} name={'Reaction'} info={'80/100'} Color="hsl(0, 100%, 67%, 30%)" />
+                <InnerComp image={'/assets/images/icon-memory.svg'} name={'Memory'} info={'96/100'} Color="hsl(39, 100%, 56%, 30%)" />
+                <InnerComp image={'/assets/images/icon-verbal.svg'} name={'Varbal'} info={'61/100'} Color="hsl(166, 100%, 37%, 30%)" />
+                <InnerComp image={'/assets/images/icon-visual.svg'} name={'Visual'} info={'73/100'} Color="hsl(234, 85%, 45%, 30%)" />
                 <button className='w-75 h-10 bg-blue-800 rounded-4xl p-2 text-white'>Continue</button>
             </section>
         </div>
